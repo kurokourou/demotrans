54f19b5c9d68048adac24491c8979886ab593b3f
bleak <kurokourou@gmail.com>
2020-06-27 13:39:37 +0700

    Update new

--- corebooster/core_booster.py
+++ corebooster/core_booster.py
@@ -14,20 +14,47 @@ EOS = text_encoder.EOS_ID
 
 # 40k sentence pairs
 # Core sentence extracted from EVBNews 2.0, a part of EVBCorpus
-# https://github.com/qhungngo/EVBCorpus
-_COREBOOSTER_TRAIN_DATASETS = [[ # Fix
-    "https://github.com/kurokourou/demotrans/raw/master/data/corebooster/corebooster_train.tar.gz",  
+# https://github.com/qhungngo/EVBCorpus # Done
+_OUT_BIAS_COREBOOSTER_TRAIN_DATASETS = [[ 
+    "https://github.com/kurokourou/demotrans/raw/master/data/corebooster/out_ci_bias/corebooster_train.tar.gz",  
     ("EVBNews_2_0_corebooster.input", "EVBNews_2_0_corebooster.output")
 ]]
 
 # 5105 sentence pairs used for test
-_COREBOOSTER_TEST_DATASETS = [[ # Fix
-    "https://github.com/kurokourou/demotrans/raw/master/data/corebooster/corebooster_test.tar.gz",  
+_OUT_BIAS_COREBOOSTER_TEST_DATASETS = [[ 
+    "https://github.com/kurokourou/demotrans/raw/master/data/corebooster/out_ci_bias/corebooster_test.tar.gz",  
     ("corebooster_test.input", "corebooster_test.output")
 ]]
 
+  # 40k sentence pairs # Fixing
+  # English core sentence extracted from EVBNews 2.0, a part of EVBCorpus
+  # https://github.com/qhungngo/EVBCorpus
+  # Core Translate by Core2core_base
+# _OUT_BALANCE_COREBOOSTER_TRAIN_DATASETS = [[ # Fix
+#     "https://github.com/kurokourou/demotrans/raw/master/data/corebooster/corebooster_train.tar.gz",  
+#     ("EVBNews_2_0_corebooster.input", "EVBNews_2_0_corebooster.output")
+# ]]
+
+# # 5105 sentence pairs used for test
+# _OUT_BALANCE_COREBOOSTER_TEST_DATASETS = [[ # Fix
+#     "https://github.com/kurokourou/demotrans/raw/master/data/corebooster/corebooster_test.tar.gz",  
+#     ("corebooster_test.input", "corebooster_test.output")
+# ]]
+
+# Translate IWLST32 with core sentence intergrated input # Done
+_IN_COREBOOSTER_TRAIN_DATASETS = [[ 
+    "https://github.com/kurokourou/demotrans/raw/master/data/corebooster/in_ci/train_core.tar.gz",  
+    ("train_core.en", "train.vi")
+]]
+
+# 5105 sentence pairs used for test
+_IN_COREBOOSTER_TEST_DATASETS = [[ 
+    "https://github.com/kurokourou/demotrans/raw/master/data/corebooster/in_ci/tst2013_core.tar.gz",  
+    ("tst2013_core.en", "tst2013.vi")
+]]
+
 @registry.register_problem
-class CoreBoostingVi(translate.TranslateProblem):
+class CoreBoosting_OutBias(translate.TranslateProblem):
   """ Vietnamese Sentence Sharpener"""
   
   @property
@@ -42,4 +69,42 @@ class CoreBoostingVi(translate.TranslateProblem):
 
   def source_data_files(self, dataset_split):
     train = dataset_split == problem.DatasetSplit.TRAIN
-    return _COREBOOSTER_TRAIN_DATASETS if train else _COREBOOSTER_TEST_DATASETS
+    return _OUT_BIAS_COREBOOSTER_TRAIN_DATASETS if train else _OUT_BIAS_COREBOOSTER_TEST_DATASETS
+
+#Fixing
+@registry.register_problem
+class CoreBoosting_OutBalance(translate.TranslateProblem):
+  """ Vietnamese Sentence Sharpener"""
+  
+  @property
+  def approx_vocab_size(self):
+    return 2**15  
+
+  @property
+  def is_generate_per_split(self):
+    # generate_data will shard the data into TRAIN and EVAL for us.
+    return False
+
+
+  def source_data_files(self, dataset_split):
+    train = dataset_split == problem.DatasetSplit.TRAIN
+    return _OUT_BALANCE_COREBOOSTER_TRAIN_DATASETS if train else _OUT_BALANCE_COREBOOSTER_TEST_DATASETS
+
+#Fixing
+@registry.register_problem
+class CoreBoosting_In(translate.TranslateProblem):
+  """ Vietnamese Sentence Sharpener"""
+  
+  @property
+  def approx_vocab_size(self):
+    return 2**15  
+
+  @property
+  def is_generate_per_split(self):
+    # generate_data will shard the data into TRAIN and EVAL for us.
+    return False
+
+
+  def source_data_files(self, dataset_split):
+    train = dataset_split == problem.DatasetSplit.TRAIN
+    return _IN_COREBOOSTER_TRAIN_DATASETS if train else _IN_COREBOOSTER_TEST_DATASETS    
